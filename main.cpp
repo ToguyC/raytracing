9e45342c44847ec54fdd27aaa7040a7bf602e6ea
ToguyC <tanguy.cvgn@gmail.com>
2024-03-09 08:52:28 +0100

    update: first sphere collision

--- main.cpp
+++ main.cpp
@@ -3,10 +3,24 @@
 #include "src/vec3.hpp"
 #include <iostream>
 
+bool hit_sphere(const point3 &center, double radius, const ray &r) {
+    vec3 oc = r.origin() - center;
+    double a = dot(r.direction(), r.direction());
+    double b = 2.0 * dot(oc, r.direction());
+    double c = dot(oc, oc) - radius * radius;
+    double discriminant = b * b - 4 * a * c;
+
+    return discriminant >= 0;
+}
+
 color ray_color(const ray &r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     double a = 0.5 * (unit_direction.y() + 1.0);
- 
+
     // lerp function
     // blendedValue = (1 - a) * startValue + a * endValue
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
