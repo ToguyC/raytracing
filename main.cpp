fe72c830a079dfd169d6c26424c03b2661c3014b
ToguyC <tanguy.cvgn@gmail.com>
2024-03-09 08:46:09 +0100

    update: first rays with linera gradient on y

--- main.cpp
+++ main.cpp
@@ -1,22 +1,59 @@
 #include "src/color.hpp"
+#include "src/ray.hpp"
 #include "src/vec3.hpp"
 #include <iostream>
 
+color ray_color(const ray &r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    double a = 0.5 * (unit_direction.y() + 1.0);
+ 
+    // lerp function
+    // blendedValue = (1 - a) * startValue + a * endValue
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
 int main() {
     // image
-    int image_width = 256;
-    int image_height = 256;
+    double aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+
+    // calculate the image height, and ensure that it's at least 1
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // camera
+    double focal_length = 1.0;
+    double viewport_height = 2.0;
+    double viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
+    point3 camera_center = point3(0, 0, 0);
+
+    // calculate the vectors across the horizontal and down the vertical
+    // viewport edges
+    vec3 viewport_u = vec3(viewport_width, 0, 0);
+    vec3 viewport_v = vec3(0, -viewport_height, 0);
+
+    // calculate the horizontal and vertical delta vectors from pixel to pixel
+    vec3 pixel_delta_u = viewport_u / image_width;
+    vec3 pixel_delta_v = viewport_v / image_height;
+
+    // calculate the location of the upper left pixel
+    vec3 viewport_upper_left =
+        camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+    vec3 pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
-        std::clog << "\rScanlines remainging: " << (image_height - j) << ' '
-                  << std::flush;
+        std::clog << "\rScanlines remainging: " << (image_height - j) << ' ' << std::flush;
 
         for (int i = 0; i < image_width; i++) {
-            color pixel = color(double(i) / (image_width - 1),
-                                double(j) / (image_height - 1), 0);
+            vec3 pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            vec3 ray_direction = pixel_center - camera_center;
+
+            ray r(camera_center, ray_direction);
+
+            color pixel = ray_color(r);
             write_color(std::cout, pixel);
         }
     }
--- src/vec3.hpp
+++ src/vec3.hpp
@@ -39,9 +39,7 @@ class vec3 {
 
     vec3 &operator/=(double t) { return *this *= 1 / t; }
 
-    double length_squared() const {
-        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
-    }
+    double length_squared() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
 
     double length() const { return sqrt(length_squared()); }
 };
@@ -76,13 +74,19 @@ inline vec3 operator*(vec3 v, double t) { return t * v; }
 inline vec3 operator/(vec3 v, double t) { return (1 / t) * v; }
 
 inline double dot(const vec3 &u, const vec3 &v) {
-    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+    // clang-format off
+    return u.e[0] * v.e[0] +
+           u.e[1] * v.e[1] +
+           u.e[2] * v.e[2];
+    // clang-format om
 }
 
 inline vec3 corss(const vec3 &u, const vec3 &v) {
+    // clang-format off
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
                 u.e[2] * v.e[0] - u.e[0] * v.e[2],
                 u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+    // clang-format om
 }
 
 inline vec3 unit_vector(vec3 v) { return v / v.length(); }
