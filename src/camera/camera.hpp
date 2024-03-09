0c5b6d79d0657fdc9f98fceb3b6333bd27493c68
ToguyC <tanguy.cvgn@gmail.com>
2024-03-09 10:29:08 +0100

    update: camera added

---
+++ src/camera/camera.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../utilities.hpp"
+
+#include "../color.hpp"
+#include "../interfaces/hittable.hpp"
+
+#include <iostream>
+
+class camera {
+  public:
+    void render(const hittable &world);
+
+  private:
+    void initialize();
+    color ray_color(const ray &r, const hittable &world) const;
+
+  public:
+    double aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+
+  private:
+    int image_height;   // Rendered image height
+    point3 center;      // Camera center
+    point3 pixel00_loc; // Location of pixel 0, 0
+    vec3 pixel_delta_u; // Offset to pixel to the right
+    vec3 pixel_delta_v; // Offset to pixel below
+};
--- src/color.hpp
+++ src/color.hpp
@@ -5,9 +5,11 @@
 
 using color = vec3;
 
-void write_color(std::ostream &out, color pixel) {
+inline void write_color(std::ostream &out, color pixel) {
     // write the translated [0,255] value of each color component
+    // clang-format off
     out << static_cast<int>(255.999 * pixel.x()) << ' '
         << static_cast<int>(255.999 * pixel.y()) << ' '
         << static_cast<int>(255.999 * pixel.z()) << '\n';
+    // clang-format on
 }
